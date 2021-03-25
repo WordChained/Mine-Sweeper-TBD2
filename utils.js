63988d130418efc41dc037058a6bbdea7a335277
WordChained <81348825+WordChained@users.noreply.github.com>
2021-03-25 08:53:16 +0200

    Add files via upload

---
+++ utils.js
@@ -0,0 +1,89 @@
+'use strict';
+
+function printMat(mat, selector) {
+    var strHTML = '<table border="0"><tbody>';
+    for (var i = 0; i < mat.length; i++) {
+        strHTML += '<tr>';
+        for (var j = 0; j < mat[0].length; j++) {
+            var cell = mat[i][j];
+            var className = 'cell cell' + i + '-' + j;
+            strHTML += '<td class="' + className + '"> ' + cell + ' </td>'
+        }
+        strHTML += '</tr>'
+    }
+    strHTML += '</tbody></table>';
+    var elContainer = document.querySelector(selector);
+    elContainer.innerHTML = strHTML;
+}
+
+// location such as: {i: 2, j: 7}
+function renderCell(location, value) {
+    // Select the elCell and set the value
+    var elCell = document.querySelector(`.cell${location.i}-${location.j}`);
+    elCell.innerHTML = value;
+}
+
+function getRandomIntInclusive(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// fix this to be a reusable neighbours loop:
+
+function countAvailable(pos) {
+    var count = 0
+    for (var i = pos.i - 1; i <= pos.i + 1; i++) {
+        if (i < 0 || i >= gCinema.length) continue;
+        for (var j = pos.j - 1; j <= pos.j + 1; j++) {
+            if (j < 0 || j >= gCinema[0].length) continue;
+            if (i === pos.i && j === pos.j) continue;
+
+            var cell = gCinema[i][j]
+            if (cell.type === 'SEAT' && !cell.isBooked) count++;
+        }
+    }
+    return count;
+}
+
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+
+function shuffle(items) {
+    var randIdx, keep, i;
+    for (i = items.length - 1; i > 0; i--) {
+        randIdx = getRandomIntInclusive(0, items.length - 1);
+
+        keep = items[i];
+        items[i] = items[randIdx];
+        items[randIdx] = keep;
+    }
+    return items;
+}
+
+
+function range(start, end) {
+    return Array(end - start + 1).fill().map((_, idx) => start + idx)
+}
+
+function getCellCoord(strCellId) {
+    var parts = strCellId.split('-')
+    var coord = { i: +parts[1], j: +parts[2] };
+    return coord;
+}
+
+function getClassName(location) {
+    var cellClass = 'cell-' + location.i + '-' + location.j;
+    return cellClass;
+}
+function getCellCoord(strCellId) {
+    var parts = strCellId.split('-')
+    var coord = { i: +parts[1], j: +parts[2] };
+    return coord;
+}
